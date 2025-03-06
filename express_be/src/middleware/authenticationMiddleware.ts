d73da675d16ef366c2ae206a0866a7f001bb3069
Ronnie F. Estillero || BSIT-2B <124552782+Roo0nnie@users.noreply.github.com>
2025-03-06 20:14:38 +0800

    Delete express_be/src directory

--- express_be/src/middleware/authenticationMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export function isAuthenticated(req: Request, res: Response, next: NextFunction): void {
-    if (req.session) {
-        next()
-    }else{
-        res.status(401).json({ errors: "Unauthorized" });
-    }
-};
