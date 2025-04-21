2f2d998d5e3a3dae0d9d74340fdd8ce6f566a920
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-04-21 15:13:23 +0800

    Feedback Connections

--- express_be/src/models/feedbackModel.ts
+++ express_be/src/models/feedbackModel.ts
@@ -56,6 +56,43 @@ class FeedbackModel{
 
         return taskReviewData;
     }
-}
+
+    static async getTaskerFeedback(){
+        const { data, error } = await supabase
+        .from("task_reviews")
+        .select(`
+            created_at,
+            feedback,
+            rating,
+            reported,
+            task_taken_id,
+            tasker:tasker_id (
+                tasker_id,
+                user:user_id (
+                    first_name,
+                    middle_name,
+                    last_name
+                )
+            ),
+            task_taken:task_taken_id (
+                client:client_id (
+                    user:user_id (
+                        first_name,
+                        middle_name,
+                        last_name
+                    )
+                )
+            )
+        `)
+        if (error) {
+            throw error;
+        }
+
+        return data.map((feedback: any) => ({
+            ...feedback,
+            created_at: feedback.created_at ? new Date(feedback.created_at).toLocaleString("en-US", { timeZone: "Asia/Manila" }) : null,
+        }));
+    }
+  }
 
 export default FeedbackModel;
